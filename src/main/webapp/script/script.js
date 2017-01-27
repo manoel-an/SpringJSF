bc9db59d6c78ec70f6688e7ef843b6bac9f61ec1
manoel-an <manoel-an@pc-manoel-an>
2017-01-26 22:51:06 -0200

    input qtd

---
+++ src/main/webapp/script/script.js
@@ -0,0 +1,18 @@
+function SomenteNumero(e) {
+	var tecla = (window.event) ? event.keyCode : e.which;
+	if ((tecla > 47 && tecla < 58))
+		return true;
+	else {
+		if (tecla == 8 || tecla == 0)
+			return true;
+		else
+			return false;
+	}
+}
+
+function validaNumero(num) {
+	if(num.value == 0){
+		document.getElementById('form:quantidade').value = 1;
+	}
+	
+}
